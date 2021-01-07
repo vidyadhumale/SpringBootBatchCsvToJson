caec593fe318681034dd9620bd1ba85a7c15a8fa
Admin <Admin@DESKTOP-1P48CVU>
2021-01-07 13:44:54 +0800

    Initial project structure check-in

---
+++ spring-batch-csv-to-json/src/main/java/springbatchcsvtojson/springbatchcsvtojson/SpringBatchCsvToJsonApplication.java
@@ -0,0 +1,13 @@
+package springbatchcsvtojson.springbatchcsvtojson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchCsvToJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchCsvToJsonApplication.class, args);
+	}
+
+}
---
+++ spring-batch-csv-to-json/src/test/java/springbatchcsvtojson/springbatchcsvtojson/SpringBatchCsvToJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package springbatchcsvtojson.springbatchcsvtojson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchCsvToJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
